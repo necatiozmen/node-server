88d38609536bd67ac45e261bf7ef3d0c96b639ca
necatiozmen <necati.ozmen.9@gmail.com>
2020-09-13 22:15:46 +0300

    Add express

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+app.use(cors());
+
+app.get("/users", (req, res) => {
+  return res.send("GET HTTP method on user resource");
+});
+
+app.post("/users", (req, res) => {
+  return res.send("POST HTTP method on user resource");
+});
+
+app.put('/users/:userId', (req, res) => {
+    return res.send(
+      `PUT HTTP method on user/${req.params.userId} resource`,
+    );
+  });
+  
+
+
+app.delete("/users", (req, res) => {
+  return res.send("DELETE HTTP method on user resource");
+});
+
+app.listen(process.env.PORT, () => console.log("Listening on port 3000"));
